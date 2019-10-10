87c5caa3ba6b52fcfa8f1c108ff0d7a9480054ce
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-10-10 17:53:36 +0300

    added market maker folder

--- custom_strategy.py
+++ custom_strategy.py
@@ -11,33 +11,32 @@ import numpy as np
 
 import matplotlib.ticker as ticker
 
-style.use('dark_background')
-mpl.rcParams['toolbar'] = 'None'
-
+style.use("dark_background")
+mpl.rcParams["toolbar"] = "None"
 
 
 class CustomOrderManager(OrderManager):
     """A sample order manager for implementing your own custom strategy"""
+
     def animate(self, i) -> None:
         xs.append(i)
-        y1 = self.exchange.get_instrument()['lastPrice']
-        y2 = self.exchange.get_instrument()['openInterest']/1000000
+        y1 = self.exchange.get_instrument()["lastPrice"]
+        y2 = self.exchange.get_instrument()["openInterest"] / 1000000
         ys1.append(y1)
         ys2.append(y2)
-        
+
         # ax1.plot(xs, ys1, color='g')
         # ax1.plot(xs, ys2, color='g')
 
-
-        color = 'tab:green'
+        color = "tab:green"
         ax1[0].ticklabel_format(useOffset=False)
-        ax1[0].set_ylabel('Price', color=color)
-        ax1[0].grid(color='w', linestyle='-', linewidth=0.1)
+        ax1[0].set_ylabel("Price", color=color)
+        ax1[0].grid(color="w", linestyle="-", linewidth=0.1)
         ax1[0].plot(xs, ys1, color=color, linewidth=1)
-        ax1[0].tick_params(axis='y', labelcolor=color)
-        
-         # instantiate a second axes that shares the same x-axis
-       
+        ax1[0].tick_params(axis="y", labelcolor=color)
+
+        # instantiate a second axes that shares the same x-axis
+
         # color = 'tab:blue'
         # ax2.ticklabel_format(useOffset=False)
         # ax2.set_ylabel('sin', color=color)
@@ -45,77 +44,73 @@ class CustomOrderManager(OrderManager):
         # ax2.plot(xs, ys2, color=color)
         # ax2.tick_params(axis='y', labelcolor=color)
 
-        #save last 50 ticks
-        ax1[0].set_xlim(left=max(0, i-720), right=i+5)
+        # save last 50 ticks
+        ax1[0].set_xlim(left=max(0, i - 720), right=i + 5)
 
-        color = 'tab:red'
+        color = "tab:red"
         ax1[1].ticklabel_format(useOffset=False)
-        ax1[1].set_xlabel('time (s)')
-        
-        ax1[1].set_ylabel('Open Interest', color=color)
+        ax1[1].set_xlabel("time (s)")
+
+        ax1[1].set_ylabel("Open Interest", color=color)
         ax1[1].plot(xs, ys2, color=color, linewidth=0.5)
-        ax1[1].tick_params(axis='y', labelcolor=color)
+        ax1[1].tick_params(axis="y", labelcolor=color)
         # start, end = ax1[1].get_xlim()
         # ax1[1].yaxis.set_ticks(np.arange(start, end, 1e9))
-       # ax1[1].yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
-        ax1[1].set_xlim(left=max(0, i-120), right=i+5)
+        # ax1[1].yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
+        ax1[1].set_xlim(left=max(0, i - 120), right=i + 5)
         # ax2.set_xlim(left=max(0, i-50), right=i+5)
         # ax2.set_xlim(left=max(0, i-50), right=i+5)
         # ax1.get_xaxis().get_major_formatter().set_scientific(False)
         # ax2.get_xaxis().get_major_formatter().set_scientific(False)
-        ax1[1].grid(color='w', linestyle='-', linewidth=0.1)
+        ax1[1].grid(color="w", linestyle="-", linewidth=0.1)
         plt.tight_layout()
         # plt.xlabel('Time(s)')
         # plt.ylabel('Open Interest')
         # plt.title('OI chart')
 
     def place_orders(self) -> None:
-         ani = animation.FuncAnimation(fig, self.animate, interval = 5000)
-         plt.show()
-       
+        ani = animation.FuncAnimation(fig, self.animate, interval=5000)
+        plt.show()
 
-
-      
     def run_loop11(self) -> None:
         while True:
             sys.stdout.write("-----\n")
             sys.stdout.flush()
 
             self.check_file_change()
-            sleep(settings.LOOP_INTERVAL)
+            sleep(1)
 
             # This will restart on very short downtime, but if it's longer,
             # the MM will crash entirely as it is unable to connect to the WS on boot.
             if not self.check_connection():
-                logger.error("Realtime data connection unexpectedly closed, restarting.")
+                logger.error(
+                    "Realtime data connection unexpectedly closed, restarting."
+                )
                 self.restart()
-
-            self.sanity_check()  # Ensures health of mm - several cut-out points here
-            self.print_status()
-           
-             # Print skew, delta, etc
+            # Print skew, delta, etc
 
 
-            
-            
-  # Creates desired orders and converges to existing orders
+# Creates desired orders and converges to existing orders
 
 
 # fig = plt.figure()
 
 fig, ax1 = plt.subplots(2)
 # ax2 = ax1.twinx()
-# ax2 = ax1.twinx() 
+# ax2 = ax1.twinx()
 xs = []
 ys1 = []
 ys2 = []
+
+
 def run_program() -> None:
-    order_manager = CustomOrderManager()    
+    order_manager = CustomOrderManager()
     # Try/except just keeps ctrl-c from printing an ugly stacktrace
     try:
         order_manager.run_loop11()
     except (KeyboardInterrupt, SystemExit):
         sys.exit()
+
 
 if __name__ == "__main__":
     run_program()
---
+++ market_maker/market_maker.py
@@ -0,0 +1,164 @@
+from __future__ import absolute_import
+from time import sleep
+import sys
+from datetime import datetime
+from os.path import getmtime
+import random
+import requests
+import atexit
+import signal
+
+from market_maker import bitmex
+from market_maker.settings import settings
+from market_maker.utils import log, constants, errors, math
+
+# Used for reloading the bot - saves modified times of key files
+import os
+
+watched_files_mtimes = [(f, getmtime(f)) for f in settings.WATCHED_FILES]
+
+
+#
+# Helpers
+#
+logger = log.setup_custom_logger("root")
+
+
+class ExchangeInterface:
+    def __init__(self, dry_run=False):
+        self.dry_run = dry_run
+        if len(sys.argv) > 1:
+            self.symbol = sys.argv[1]
+        else:
+            self.symbol = settings.SYMBOL
+        self.bitmex = bitmex.BitMEX(
+            base_url=settings.BASE_URL,
+            symbol=self.symbol,
+            apiKey=settings.API_KEY,
+            apiSecret=settings.API_SECRET,
+            orderIDPrefix=settings.ORDERID_PREFIX,
+            postOnly=settings.POST_ONLY,
+            timeout=settings.TIMEOUT,
+        )
+
+    def get_instrument(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.instrument(symbol)
+
+    def get_margin(self):
+        if self.dry_run:
+            return {
+                "marginBalance": float(settings.DRY_BTC),
+                "availableFunds": float(settings.DRY_BTC),
+            }
+        return self.bitmex.funds()
+
+    def get_position(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.position(symbol)
+
+    def get_ticker(self, symbol=None):
+        if symbol is None:
+            symbol = self.symbol
+        return self.bitmex.ticker_data(symbol)
+
+    def is_open(self):
+        """Check that websockets are still open."""
+        return not self.bitmex.ws.exited
+
+    def check_market_open(self):
+        instrument = self.get_instrument()
+        if instrument["state"] != "Open" and instrument["state"] != "Closed":
+            raise errors.MarketClosedError(
+                "The instrument %s is not open. State: %s"
+                % (self.symbol, instrument["state"])
+            )
+
+    def check_if_orderbook_empty(self):
+        """This function checks whether the order book is empty"""
+        instrument = self.get_instrument()
+        if instrument["midPrice"] is None:
+            raise errors.MarketEmptyError("Orderbook is empty, cannot quote")
+
+    def cancel_bulk_orders(self, orders):
+        if self.dry_run:
+            return orders
+        return self.bitmex.cancel([order["orderID"] for order in orders])
+
+
+class OrderManager:
+    def __init__(self):
+        self.exchange = ExchangeInterface(settings.DRY_RUN)
+        # Once exchange is created, register exit handler that will always cancel orders
+        # on any error.
+        atexit.register(self.exit)
+        signal.signal(signal.SIGTERM, self.exit)
+
+        logger.info("Using symbol %s." % self.exchange.symbol)
+
+        logger.info(
+            "Order Manager initializing, connecting to BitMEX. Live run: executing real trades."
+        )
+
+        self.start_time = datetime.now()
+        self.instrument = self.exchange.get_instrument()
+        self.reset()
+
+    def reset(self):
+        # Create orders and converge.
+        self.place_orders()
+
+    ###
+    # Orders
+    ###
+
+    def place_orders(self):
+        """Create order items for use in convergence."""
+        pass
+
+    def check_file_change(self):
+        """Restart if any files we're watching have changed."""
+        for f, mtime in watched_files_mtimes:
+            if getmtime(f) > mtime:
+                self.restart()
+
+    def check_connection(self):
+        """Ensure the WS connections are still open."""
+        return self.exchange.is_open()
+
+    def exit(self):
+        logger.info("Shutting down. All open orders will be cancelled.")
+        try:
+            self.exchange.bitmex.exit()
+        except errors.AuthenticationError as e:
+            logger.info("Was not authenticated; could not cancel orders.")
+        except Exception as e:
+            logger.info("Unable to cancel orders: %s" % e)
+
+        sys.exit()
+
+    def restart(self):
+        logger.info("Restarting the market maker...")
+        os.execv(sys.executable, [sys.executable] + sys.argv)
+
+
+#
+# Helpers
+#
+
+
+def XBt_to_XBT(XBt):
+    return float(XBt) / constants.XBt_TO_XBT
+
+
+def run():
+    logger.info("BitMEX Market Maker Version: %s\n" % constants.VERSION)
+
+    om = OrderManager()
+    # Try/except just keeps ctrl-c from printing an ugly stacktrace
+    try:
+        om.run_loop()
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
