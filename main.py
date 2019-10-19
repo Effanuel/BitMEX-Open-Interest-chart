31d50a5aab51367c5a823e249f7ff2f25b529e6d
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-10-20 00:01:47 +0300

    Clarification of functions

--- main.py
+++ main.py
@@ -10,24 +10,35 @@ from matplotlib import style
 
 import matplotlib.ticker as ticker
 
+
+# Style adjustments for matplotlib
 style.use("dark_background")
 mpl.rcParams["toolbar"] = "None"
 
+# Figures
+fig, ax1 = plt.subplots(2)
+# Arrays to display data using matplotlib
+xs = []
+ys1 = []
+ys2 = []
 
+
+# Inheritance from sample-market-maker class OrderManager
 class Chart(OrderManager):
-    """A sample order manager for implementing your own custom strategy"""
+    """A class to initialize charting. Inheritance from sample-market-maker class OrderManager"""
 
     def animate(self, i) -> None:
+
+        # On each iteration appends values to an array to display later
         xs.append(i)
+        # Gets current price of XBTUSD
         y1 = self.exchange.get_instrument()["lastPrice"]
+        # Gets Open Interest of XBTUSD
         y2 = self.exchange.get_instrument()["openInterest"] / 1000000
         ys1.append(y1)
         ys2.append(y2)
 
-        # ax1.plot(xs, ys1, color='g')
-        # ax1.plot(xs, ys2, color='g')
-
-        # PRICE CHARTING
+        # Colors for the chart
         color_arr = [
             "tab:blue",
             "tab:orange",
@@ -40,90 +51,58 @@ class Chart(OrderManager):
             "tab:olive",
             "tab:cyan",
         ]
-
+        # Get price line color from settings
         if settings.PRICE_LINE_COLOR in color_arr:
             price_color = settings.PRICE_LINE_COLOR
         else:
             price_color = "green"
-
-        ax1[0].ticklabel_format(useOffset=False)
-        ax1[0].set_ylabel("Price", color=price_color)
-        ax1[0].grid(color="w", linestyle="-", linewidth=0.1)
-        ax1[0].plot(xs, ys1, color=price_color, linewidth=1)
-        ax1[0].tick_params(axis="y", labelcolor=price_color)
-
-        # instantiate a second axes that shares the same x-axis
-
-        # color = 'tab:blue'
-        # ax2.ticklabel_format(useOffset=False)
-        # ax2.set_ylabel('sin', color=color)
-        #   # we already handled the x-label with ax1
-        # ax2.plot(xs, ys2, color=color)
-        # ax2.tick_params(axis='y', labelcolor=color)
-
-        # save last 50 ticks
-
+        # Get interest line color from settings
+        if settings.INTEREST_LINE_COLOR in color_arr:
+            interest_color = settings.INTEREST_LINE_COLOR
+        else:
+            interest_color = "red"
+        # Get line save time from settings
         if settings.SAVE_CHART_IN_SECONDS >= 300:  # 5 minutes
             save_time = settings.SAVE_CHART_IN_SECONDS / 5
         else:
             save_time = 300 / 5
 
+        # INIT PRICE CHART
+        ax1[0].ticklabel_format(useOffset=False)
+        ax1[0].set_ylabel("Price", color=price_color)
+        ax1[0].grid(color="w", linestyle="-", linewidth=0.1)
+        ax1[0].plot(xs, ys1, color=price_color, linewidth=1)
+        ax1[0].tick_params(axis="y", labelcolor=price_color)
         ax1[0].set_xlim(left=max(0, i - save_time), right=i + 5)
 
-        if settings.INTEREST_LINE_COLOR in color_arr:
-            interest_color = settings.INTEREST_LINE_COLOR
-        else:
-            interest_color = "red"
-
+        # INIT OPEN-INTEREST CHART
         ax1[1].ticklabel_format(useOffset=False)
         ax1[1].set_xlabel("ticks (1 tick = 5s)", color=interest_color)
-
         ax1[1].set_ylabel("Open Interest", color=interest_color)
         ax1[1].plot(xs, ys2, color=interest_color, linewidth=0.5)
         ax1[1].tick_params(axis="y", labelcolor=interest_color)
-
         ax1[1].set_xlim(left=max(0, i - save_time), right=i + 5)
         ax1[1].grid(color="w", linestyle="-", linewidth=0.1)
+
         plt.tight_layout()
 
-    def place_orders(self) -> None:
+    def chart(self) -> None:
         ani = animation.FuncAnimation(fig, self.animate, interval=5000)
         plt.show()
 
     def start(self) -> None:
-        while True:
-            sys.stdout.write("-----\n")
-            sys.stdout.flush()
-
-            self.check_file_change()
-            sleep(1)
-
-            # This will restart on very short downtime, but if it's longer,
-            # the MM will crash entirely as it is unable to connect to the WS on boot.
-            if not self.check_connection():
-                logger.error(
-                    "Realtime data connection unexpectedly closed, restarting."
-                )
-                self.restart()
-            # Print skew, delta, etc
+        pass
 
 
-# Creates desired orders and converges to existing orders
-# fig = plt.figure()
-fig, ax1 = plt.subplots(2)
-# ax2 = ax1.twinx()
-# ax2 = ax1.twinx()
-xs = []
-ys1 = []
-ys2 = []
-
-
+# MAIN FUNCTION
 def run_program() -> None:
-    chart = Chart()
-    # Try/except just keeps ctrl-c from printing an ugly stacktrace
+    """
+    This class initialization calls constructor inherited OrderManager
+    which calls chart function
+    """
     try:
-        sys.exit()
-        chart.start()
+        chart = Chart()  # Is alive while the chart window is opened
+        sys.exit()  # Closes the program if chart window is closed
     except (KeyboardInterrupt, SystemExit):
         sys.exit()
 
