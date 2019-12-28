d2557214da0afbee69d8efd648af1366b54b6d91
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-12-28 16:00:39 +0200

    Fixed default save time

--- settings.py
+++ settings.py
@@ -4,19 +4,19 @@ import logging
 
 # True or False
 EXPORT_DATA_TO_FILE_AFTER_CHART_CLOSE = False
+# File for exporting data
+EXPORT_TO_FILE_NAME = 'data.csv'
 
 # True or False // Currently doesnt work with live data
 ENABLE_VERTICAL_LINES_ON_HOVER = True
 ENABLE_HORIZONTAL_LINES_ON_HOVER = False
-EXPORT_TO_FILE_NAME = 'data.csv'
-
 
 # API URL.
 BASE_URL = "https://www.bitmex.com/api/v1/"
 
 # What is the maximum duration of price/interest you can see on the chart
-# Default saves 1 hour of time on the chart
-SAVE_CHART_IN_SECONDS = 3600
+# Default saves 30 minutes of time on the chart
+SAVE_CHART_IN_SECONDS = 1800
 
 # Charting color of price and interest
 # Available colors :
