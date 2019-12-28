3b8201cb9e9a0a6394225de2a6e75df2598cb83b
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-12-28 15:49:23 +0200

    Removed api settings, changed default color

--- settings.py
+++ settings.py
@@ -8,18 +8,11 @@ EXPORT_DATA_TO_FILE_AFTER_CHART_CLOSE = False
 # True or False // Currently doesnt work with live data
 ENABLE_VERTICAL_LINES_ON_HOVER = True
 ENABLE_HORIZONTAL_LINES_ON_HOVER = False
+EXPORT_TO_FILE_NAME = 'data.csv'
 
 
 # API URL.
 BASE_URL = "https://www.bitmex.com/api/v1/"
-
-########################################################################################################################
-# Connection/Auth
-########################################################################################################################
-
-# The BitMEX API requires permanent API keys. Go to https://www.bitmex.com/app/apiKeys to fill these out.
-API_KEY = ""
-API_SECRET = ""
 
 # What is the maximum duration of price/interest you can see on the chart
 # Default saves 1 hour of time on the chart
@@ -38,7 +31,7 @@ SAVE_CHART_IN_SECONDS = 3600
 # "olive"
 # "cyan"
 PRICE_LINE_COLOR = "green"
-INTEREST_LINE_COLOR = "red"
+INTEREST_LINE_COLOR = "orange"
 HOVER_LINE_COLOR = "cyan"
 
 # ---
